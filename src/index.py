f8e600c5cf7257d37d77999b290a4449fb87037a
rautiais <rautiainen.e.salla@gmail.com>
2024-11-11 11:41:28 +0200

    Viikko2 Teht 4

--- src/index.py
+++ src/index.py
@@ -1,19 +1,22 @@
 from varasto import Varasto
 
-
-def main():
+def luo_varastot():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
+    return mehua, olutta
 
+def tulosta_varastot(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+def olut(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def mehu(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +25,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def virhetilanteet():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,28 +35,43 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def virhetilanteet_olut(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+def virhetilanteet_mehu(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def ota_olutta(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def ota_mehua(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
 
+
+def main():
+    mehua, olutta = luo_varastot()
+    tulosta_varastot(mehua, olutta)
+    olut(olutta)
+    mehu(mehua)
+    virhetilanteet()
+    virhetilanteet_olut(olutta)
+    virhetilanteet_mehu(mehua)
+    ota_olutta(olutta)
+    ota_mehua(mehua)
 
 if __name__ == "__main__":
     main()
